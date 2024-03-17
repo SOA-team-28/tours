779449be4c917d1ab0cd1da97606e2ec463c8311
somelijer <116906162+somelijer@users.noreply.github.com>
2024-03-17 23:10:37 +0100

    feat: photo list mapping

--- WebServerWithDB/model/TourDTO.go
+++ WebServerWithDB/model/TourDTO.go
@@ -2,6 +2,21 @@ package model
 
 import "strings"
 
+type CheckpointDTO struct {
+	ID                   int     `json:"id"`
+	TourID               int     `json:"tourId"`
+	AuthorID             int     `json:"authorId"`
+	Longitude            float64 `json:"longitude"`
+	Latitude             float64 `json:"latitude"`
+	Name                 string  `json:"name"`
+	Description          string  `json:"description"`
+	Pictures             []string  `json:"pictures"`
+	RequiredTime         int     `json:"requiredTimeInSeconds"`
+	CheckpointSecret     string  `json:"checkpointSecret"`
+	EncounterID          int     `json:"encounterId"`
+	IsSecretPrerequisite bool    `json:"isSecretPrerequisite"`
+}
+
 // TourData represents the JSON data structure for creating a tour
 type TourDTO struct {
 	ID             int            `json:"id"`
@@ -13,7 +28,7 @@ type TourDTO struct {
 	AuthorID       int            `json:"authorId"`
 	Status         string         `json:"status"`
 	Equipment      []Equipment `json:"equipment"`
-	Checkpoints    []Checkpoint `json:"checkpoints"`
+	Checkpoints    []CheckpointDTO `json:"checkpoints"`
 	TourTimes      []int          `json:"tourTimes"`
 	TourRatings    []int          `json:"tourRatings"`
 	Closed         bool           `json:"closed"`
@@ -34,5 +49,32 @@ func (tourDTO *TourDTO) MapToTour() (Tour, []Equipment, []Checkpoint) {
 		Closed:         tourDTO.Closed,
 	}
 
-	return tour, tourDTO.Equipment, tourDTO.Checkpoints
+	var equipment []Equipment
+	for _, eq := range tourDTO.Equipment {
+		equipment = append(equipment, Equipment{
+			ID:          eq.ID,
+			Name:        eq.Name,
+			Description: eq.Description,
+		})
+	}
+
+	var checkpoints []Checkpoint
+	for _, cp := range tourDTO.Checkpoints {
+		checkpoints = append(checkpoints, Checkpoint{
+			ID:                   cp.ID,
+			TourID:               cp.TourID,
+			AuthorID:             cp.AuthorID,
+			Longitude:            cp.Longitude,
+			Latitude:             cp.Latitude,
+			Name:                 cp.Name,
+			Description:          cp.Description,
+			Pictures:             strings.Join(cp.Pictures, "|"), // Join pictures into a single string
+			RequiredTime:         cp.RequiredTime,
+			CheckpointSecret:     cp.CheckpointSecret,
+			EncounterID:          cp.EncounterID,
+			IsSecretPrerequisite: cp.IsSecretPrerequisite,
+		})
+	}
+
+	return tour, equipment, checkpoints
 }
