4aac2b7b2953aa6fddaefd8ea4ef7bc7feb7e55c
somelijer <116906162+somelijer@users.noreply.github.com>
2024-03-21 12:15:19 +0100

    feat: finally see end of milion tours routes

--- WebServerWithDB/service/TourService.go
+++ WebServerWithDB/service/TourService.go
@@ -48,3 +48,39 @@ func (service *TourService) Delete(id int) error {
 	}
 	return nil
 }
+
+func (service *TourService) GetToursByAuthor(authorID, page, pageSize int) ([]model.Tour, error) {
+	// Implement logic to get tours by author ID from the repository
+	tours, err := service.TourRepo.GetToursByAuthor(authorID)
+	if err != nil {
+		return nil, err
+	}
+	return tours, nil
+}
+
+func (service *TourService) GetAll(page, pageSize int) ([]model.Tour, error) {
+	// Implement logic to get all tours from the repository
+	tours, err := service.TourRepo.GetAll(page, pageSize)
+	if err != nil {
+		return nil, err
+	}
+	return tours, nil
+}
+
+func (service *TourService) AddEquipment(tourID, equipmentID int) error {
+	// Implement logic to add equipment to a tour in the repository
+	err := service.TourRepo.AddEquipment(tourID, equipmentID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (service *TourService) RemoveEquipment(tourID, equipmentID int) error {
+	// Implement logic to remove equipment from a tour in the repository
+	err := service.TourRepo.RemoveEquipment(tourID, equipmentID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
